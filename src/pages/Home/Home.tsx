f7276e40f56e07db91d425cc0ab8ef395260161d
Alberto Duque <alberto.duque@ibm.com>
2024-01-22 17:11:23 -0500

    fix(Home): update stock

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import './style.css'
 
 const Home = () => {
   const products =[
-    { alt:'tv', image:'/assets/tv.jpg', name:'Tv de 43', description:'Es un tv para toda la familia', stock:'5', price:'30000' },
+    { alt:'tv', image:'/assets/tv.jpg', name:'Tv de 43', description:'Es un tv para toda la familia', stock:'25', price:'30000' },
     { alt:'tv-lg', image:'/assets/tv2.jpg', name:'Tv Lg de 53', description:'Es un tv para toda la familia y para una mejor experiencia con videjuegos', stock:'15', price:'50000' },
     { alt:'tv-43', image:'/assets/tv4.jpg', name:'Tv de 32', description:'Es un tv para tenerlo en el mejor lugar de tu hogar', stock:'45', price:'10000' },
     { alt:'tv-54', image:'/assets/tv3.jpg', name:'Tv de 65', description:'Es un tv de 65" para toda la familia', stock:'46', price:'60000' }
