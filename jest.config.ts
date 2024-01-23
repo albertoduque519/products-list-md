be4f223e39baddf03dd291204c7e2eddc005dcb3
Alberto Duque <alberto.duque@ibm.com>
2024-01-23 08:42:25 -0500

    fix(Images): update jest config for mock url

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,22 @@ export default {
     },
   
     transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+      '^.+\\.tsx?$': [
+        'ts-jest',
+        {
+          diagnostics: {
+            ignoreCodes: [1343]
+          },
+          astTransformers: {
+            before: [
+              {
+                path: 'node_modules/ts-jest-mock-import-meta',  // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+                options: { metaObjectReplacement: { url: 'https://www.url.com' } }
+              }
+            ]
+          }
+        }
+      ]
     },
   
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
